d088cbeda150a422ef812fa29cab6d30232e845b
Vehask <141333070+Vehask@users.noreply.github.com>
2026-06-16 16:42:04 +0200

    Remove debug/deprecated scripts (v3 cleanup)

--- stop-media-monitor.ps1
+++
@@ -1,6 +0,0 @@
-Get-Process powershell | Where-Object {
-    $_.CommandLine -like "*desktop-media-monitor.ps1*"
-} | Stop-Process -Force
-
-Write-Host "Desktop Media Monitor stopped"
-Start-Sleep -Seconds 2
